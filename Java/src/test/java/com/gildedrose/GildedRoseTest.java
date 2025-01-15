ca4468277ca4881a1c36889218263e592e1da882
SZanko <szanko_dev@proton.me>
2025-01-15 14:04:51 +0100

    Add test for non valid name
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private final GildedRose app = new GildedRose(null);
+
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void GIVEN_nonValidName_WHEN_updateQualityAndQualityUnder50AndSelling0_THEN_updateQuality() {
+        final Item[] items = new Item[] { new Item("foo", 0, 49) };
+        app.items = items;
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(47, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals("foo", app.items[0].name);
     }
+
+    @Test
+    void GIVEN_nonValidName_WHEN_updateQualityAndQualityUnder0AndSelling0_THEN_updateQuality() {
+        final Item[] items = new Item[] { new Item("foo", 0, -1) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(-1, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void GIVEN_validName_WHEN_updateQualityAndQualityUnder0AndSelling0_THEN_updateQuality() {
+        final Item[] items = new Item[] { new Item("Aged Brie", 0, -1) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+    }
+
+
+
 
 }
