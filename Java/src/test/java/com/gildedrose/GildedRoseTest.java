af9d22a1781764014b21afe9f6a21d8903ce0c57
pebba <mumo.space@gmail.com>
2025-01-15 14:20:57 +0100

    add test and readme

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,6 @@ class GildedRoseTest {
 
     private final GildedRose app = new GildedRose(null);
 
-
     @Test
     void GIVEN_nonValidName_WHEN_updateQualityAndQualityUnder50AndSelling0_THEN_updateQuality() {
         final Item[] items = new Item[] { new Item("foo", 0, 49) };
@@ -25,7 +24,7 @@ class GildedRoseTest {
         app.items = items;
         app.updateQuality();
         assertEquals(-1, app.items[0].quality);
-        assertEquals(0, app.items[0].sellIn);
+        assertEquals(-1, app.items[0].sellIn);
         assertEquals("foo", app.items[0].name);
     }
 
@@ -39,7 +38,67 @@ class GildedRoseTest {
         assertEquals(1, app.items[0].quality);
     }
 
+    @Test
+    void normalItemDegradesInQuality() {
+        Item[] items = new Item[] { new Item("foo", 10, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(19, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
 
+    @Test
+    void agedBrieIncreasesInQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
 
+    @Test
+    void sulfurasNeverDecreasesInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(10, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesIncreaseInQuality() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+        assertEquals(14, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesIncreaseInQualityBy2When10DaysOrLess() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesIncreaseInQualityBy3When5DaysOrLess() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(23, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesDropToZeroAfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        app.items = items;
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
 
 }
---
+++ Java/src/test/java/com/gildedrose/readme.md
@@ -0,0 +1,8 @@
+challenges faced:
+- naming conventions for the tests
+- understanding the requirements
+- reading and understanding the code
+
+test strategy:
+- cover edgecases like non valid names, negative sellIn, negative quality, quality > 50, quality < 0
+- test the code with the requirements
